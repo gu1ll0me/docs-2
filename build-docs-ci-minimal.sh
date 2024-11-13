6e1a8c5ac0e2d3297bf8494679a51a0904434b31
Ravind Kumar <ravind@min.io>
2024-11-13 15:44:12 -0500

    Adding minimal workflow to reduce build times

--- build-docs-ci-minimal.sh
+++ build-docs-ci-minimal.sh
@@ -1,37 +1,15 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 export PATH=${PATH}:${HOME}/.local/bin
 
 
 make SYNC_SDK=TRUE linux
-make windows macos container k8s openshift eks aks gke
+make k8s
 
 mkdir -p docs/minio/kubernetes/upstream
 cp -vr build/HEAD/k8s/html/* docs/minio/kubernetes/upstream/
 
-mkdir -p docs/minio/kubernetes/eks
-cp -vr build/HEAD/eks/html/* docs/minio/kubernetes/eks/
-
-
-mkdir -p docs/minio/kubernetes/gke
-cp -vr build/HEAD/gke/html/* docs/minio/kubernetes/gke/
-
-mkdir -p docs/minio/kubernetes/aks
-cp -vr build/HEAD/aks/html/* docs/minio/kubernetes/aks/
-
-mkdir -p docs/minio/kubernetes/openshift
-cp -vr build/HEAD/openshift/html/* docs/minio/kubernetes/openshift/
-
-mkdir -p docs/minio/container
-cp -vr build/HEAD/container/html/* docs/minio/container/
-
 mkdir -p docs/minio/linux
 cp -vr build/HEAD/linux/html/* docs/minio/linux/
-
-mkdir -p docs/minio/macos
-cp -vr build/HEAD/macos/html/* docs/minio/macos/
-
-mkdir -p docs/minio/windows
-cp -vr build/HEAD/windows/html/* docs/minio/windows/
